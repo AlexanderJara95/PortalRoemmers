d94586e378c1771668b0372958dc60f1c4dc5e63
AlexanderJara95 <luisalexanderjara95@gmail.com>
2024-01-11 12:44:51 -0500

    Agregando "/" al Título

--- PortalRoemmers/Areas/Marketing/Controllers/SolicitudGastoMkt/SolicitudGastoMktController.cs
+++ PortalRoemmers/Areas/Marketing/Controllers/SolicitudGastoMkt/SolicitudGastoMktController.cs
@@ -231,7 +231,7 @@ namespace PortalRoemmers.Areas.Marketing.Controllers
             creaTablas(familiaD, medicoD, responsableD, actividadD,documentoD);
             
             ViewBag.fecha = actual;
-            ViewBag.Titulo = model.titSolGas;
+            ViewBag.Titulo = model.titSolGas.Trim()+"/";
 
             return View();
         }
